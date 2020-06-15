44794463738af9c7ab9887e591fe31c6facd10ca
MoAmine <moaminebelkhadir@gmail.com>
2020-06-15 12:19:27 +0200

    deployement

---
+++ src/app/components/elements/examples/landing/landing.module.ts
@@ -0,0 +1,26 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { RouterModule } from '@angular/router';
+import { AppRoutingModule } from '../../../../app.routing';
+
+
+
+
+@NgModule({
+  declarations: [
+  
+   
+  ],
+  imports: [
+    BrowserModule,
+    NgbModule,
+    FormsModule,
+    RouterModule,
+    AppRoutingModule,
+  ],
+  providers: [],
+  bootstrap: []
+})
+export class LandingModule { }
